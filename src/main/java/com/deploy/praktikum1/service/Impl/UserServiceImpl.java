627283d13a185e323ae3227410954f3fc5ba1153
iizaghz <sukmahawaiza@gmail.com>
2026-03-03 11:58:58 +0700

    add class UserServicelmpl dalam package service->lmpl

---
+++ src/main/java/com/deploy/praktikum1/service/Impl/UserServiceImpl.java
@@ -0,0 +1,83 @@
+package com.deploy.praktikum1.service.Impl;
+
+import com.deploy.praktikum1.mapper.UserMapper;
+import com.deploy.praktikum1.model.dto.UserAddRequest;
+import com.deploy.praktikum1.model.dto.UserDto;
+import com.deploy.praktikum1.model.entity.User;
+import com.deploy.praktikum1.repository.UserRepository;
+import com.deploy.praktikum1.service.UserService;
+import com.deploy.praktikum1.util.ValidationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ValidationUtil validationUtil;
+
+    @Override
+    public UserDto AddUser(UserAddRequest request) {
+        validationUtil.validate(request);
+
+        User saveUser = User.builder()
+                .Id(UUID.randomUUID().toString())
+                .name(request.getName())
+                .age(request.getAge())
+                .build();
+
+        userRepository.save(saveUser);
+
+        return UserMapper.MAPPER.toUserDtoData(saveUser);
+    }
+
+    @Override
+    public List<UserDto> getAllUser() {
+        List<User> users = userRepository.findAll();
+        List<UserDto> userDtos = new ArrayList<>();
+
+        for (User user : users) {
+            userDtos.add(UserMapper.MAPPER.toUserDtoData(user));
+        }
+
+        return userDtos;
+    }
+
+    @Override
+    public UserDto getUserById(String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return UserMapper.MAPPER.toUserDtoData(user);
+    }
+
+    @Override
+    public UserDto UpdateUser(String id, UserAddRequest request) {
+        validationUtil.validate(request);
+
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        existingUser.setName(request.getName());
+        existingUser.setAge(request.getAge());
+
+        userRepository.save(existingUser);
+
+        return UserMapper.MAPPER.toUserDtoData(existingUser);
+    }
+
+    @Override
+    public void DeleteUser(String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        userRepository.delete(user);
+    }
+}
